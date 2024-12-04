f4acc1188acdee7bf18671b8f9c5afe7e042d639
Maria <masha-kem133@mail.ru>
2024-12-04 14:36:41 +0100

    small fix

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,7 +4,7 @@ import { ReactComponent as Logo } from '../../assets/icons/Logo.svg';
 export const Header = () => {
   return (
     <div className={styles.header}>
-      <Logo className={styles.header__logo}></Logo>
+      <Logo />
     </div>
   );
 };

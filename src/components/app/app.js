b73318711cb7af51c71ff536cadd84c476b56988
Maria <masha-kem133@mail.ru>
2024-12-17 19:07:36 +0100

    redux

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,7 +5,12 @@ import TransferFilterPanel from '../transfer-filter-panel';
 import SortingPanel from '../sorting-panel';
 import TicketsList from '../tickets-list';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const value = useSelector((state) => state.value);
+
   return (
     <section className={styles['app']}>
       <Header />
--- src/state/store.js
+++ src/state/store.js
@@ -3,11 +3,4 @@ import { configureStore, bindActionCreators } from '@reduxjs/toolkit';
 import { reducer } from './reducer';
 import * as actions from './actions';
 
-const store = configureStore({ reducer });
-const { dispatch } = store;
-
-const { act } = bindActionCreators(actions, dispatch);
-
-const update = () => {};
-
-store.subscribe(update);
+export const store = configureStore({ reducer });
